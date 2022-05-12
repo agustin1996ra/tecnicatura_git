bce65116ffd1b3563154f624bd781aa6c6cecb5a
Agustin Rodriguez <rodriguezalvarezagustin@gmail.com>
2022-05-11 22:41:30 -0300

    Creamos el proyecto de la leccion 2

---
+++ Python/leccion2/main.py
@@ -0,0 +1,17 @@
+# En esta clase veremos la sentencia if else
+# condicion = True
+# if condicion is True:
+#     print('Condición verdadera')
+# elif condicion is False:
+#     print('Condición Falsa')
+# else:
+#     print("Condición sin especificar")
+
+# Ejecución paso a paso en el condicional
+condicion = True  # punto de ruptura
+if condicion is True:
+    print('Condición verdadera')
+elif condicion is False:
+    print('Condición Falsa')
+else:
+    print("Condición sin especificar")
