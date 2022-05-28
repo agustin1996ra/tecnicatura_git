2f88931a0352ed149296fe332cfec8ba0de08676
Agustin   Rodriguez Alvarez <63318331+agustin1996ra@users.noreply.github.com>
2022-05-27 23:14:31 -0300

    Clase 8 de programacion archivos: HolaMundo, Ejercicio_3 y Ejercicio_4

--- Java/Leccion1_copia/Introduccion/src/HolaMundo.java
+++ Java/Leccion1_copia/Introduccion/src/HolaMundo.java
@@ -183,6 +183,8 @@ public class HolaMundo {
         System.out.println("entrada = " + entrada);
         System.out.println("edad = " + edad);
         */
+        
+        /*
         // Conversión de tipos primitivos (parte dos)
         
         var edadTexto = String.valueOf(10);
@@ -195,6 +197,180 @@ public class HolaMundo {
         var entrada = new Scanner(System.in);
         fraseChar = entrada.nextLine().charAt(0); // por mas que pongamos una cadena, solo tomara el carater en el indice especificado.
         System.out.println("fraseChar = " + fraseChar);
+        */
+        /*
+        // Operadores
+        int num1 = 5, num2 = 4;
+        var solucion = num1 + num2;
+        System.out.println("solucion suma= " + solucion);
+        
+        solucion = num1 - num2;
+        System.out.println("solucion resta= " + solucion);
+        
+        solucion = num1 * num2;
+        System.out.println("solucion multiplicacion = " + solucion);
+        
+        solucion = num1 / num2;
+        System.out.println("solucion division= " + solucion);
+        
+        var solucion2 = 3.4 / num2;
+        System.out.println("solucion2 = " + solucion2);
+        */
+        /*
+        // Operadores Unarios: Cambio de signo
+        var varA = 7;
+        var varB = -varA;
+        System.out.println("varA = " + varA);
+        System.out.println("varB = " + varB); // El resultado será un nuemro negativo 
+        
+        // Operadores de negación
+        var varC = true; //Esta literal por default en Java es de tipo boolean
+        var varD = !varC; //Aquí se invierte el valor gracias al sino !
+        System.out.println("varC = " + varC);
+        System.out.println("varD = " + varD);
+        
+        //Operadores unarios de incremento: Preincremento
+        var varE = 9; // Se va a modificar su valor 
+        var varF = ++varE; // Note que el simbolo del incrememento est antes de la variable
+        // Primero se incrtementa la variable "varE" y después se asigna su valor a "varF"
+        // Por lo que las dos variables tendran el mismo valor.
+        System.out.println("varE = " + varE);// se incrementa en una unidad
+        System.out.println("varF = " + varF);// Se le asigna el valor de la otra varaiable incrementada
+        
+        // Postincremento (el simbolo va después de la variable)
+        var varG = 3;
+        var varH = varG++; // primero el valor de la variable y luego los sibolos del incremento
+        System.out.println("varG " + varG);// Tenerb en cuenta que el valor de la "varG" se incremento
+        System.out.println("varH = " + varH); // pero el valor asignado a la variable "varH" tiene
+        //tiene asignado el valor antes del incremento, por que al leer de izquierda a derecha
+        // lo primero que hace es asignar el valor, y despues incrementar la varG.
+        
+        //Operadores unarios de decremento: predecremento
+        var varI = 4;
+        var varJ = --varI;
+        System.out.println("varI = " + varI); //La variable ya esta con decremento
+        System.out.println("varJ = " + varJ); //Tendra el mismo valor que la variable "varI"
+        
+        // Postdecremento
+        var varK = 8;
+        var varL = varK--;// Priemro se asigna la variable y luego se la decrementa.
+        System.out.println("varK = " + varK);// nos mostrara el decremento de "varK"
+        System.out.println("varL = " + varL);
+        */
+        /*
+        //Opeadores de Igualdad y Relacionales
+        var aNum = 5;
+        var bNum = 4;
+        var cNum = (aNum == bNum); //operador de igualdad, resultado: true o false
+        System.out.println("cNum = " + cNum);
+        
+        var dNum = aNum != bNum; // Operador de diferentes, o no iguales.
+        System.out.println("dNum = " + dNum);
+        
+        // en este caso veremos que no sirve para comaprar cadena usar directamente el operador ==
+        var cadenaA = "Hello";
+        var cadenaB = "bye bye";
+        // Ya que solo comparara las referencias de la cadena y no su contenido
+        var cVar = cadenaA == cadenaB;
+        System.out.println("cVar = " + cVar);
+        
+        // para comaparar el contenido de los objetos de tipo string
+        // usamos ".ecuals()"
+        var fVar = cadenaA.equals(cadenaB);
+        System.out.println("fVar = " + fVar);
+        
+        var gVar = aNum > bNum;
+        System.out.println("gVar = " + gVar);
+        
+        var hVar = aNum < bNum;
+        System.out.println("hVar = " + hVar);
+        
+        var iVar = aNum >= bNum;
+        System.out.println("iVar = " + iVar);
+        
+        var jVar = aNum <= bNum;
+        System.out.println("jVar = " + jVar);
+        
+        var kVar = aNum != bNum;
+        System.out.println("kVar = " + kVar);
+        
+        if (aNum % 2 == 0) {
+            System.out.println("El numero es Par");
+        } else {
+            System.out.println("El numero es Impar");
+        }
+        
+        if (bNum % 2 == 0) {
+            System.out.println("El numero es Par");
+        } else {
+            System.out.println("El numero es Impar");
+        }
+        
+        var edad = 30;
+        var adulto = 18;
+        if (edad >= adulto) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+        */
+        
+        /*
+        // Operador condicional AND "&&"
+        var valorA = 7;
+        var valorMinimo = 0; // rango del 0 al 10
+        var valorMaximo = 10;
+        var respuesta = valorA >= 0 && valorA < 10;
+        
+        if (respuesta) {
+            System.out.println("Esta dentro del rango establecido");
+        } else {
+            System.out.println("Esta fuera del rango establecido");
+        }
+        
+        
+        var vacaciones = false;
+        var diaLibre = false;
+        if (vacaciones || diaLibre) {
+            System.out.println("El padre pede asistir al juego de su hijo");
+        } else {
+            System.out.println("El padre no puede asistir al juego de su hijo");
+        }
+        */
+        /*
+        // Operador ternario
+        var resultadoT = (5 > 4) ? "Verdadero" : "Falso";
+        // Es un operador compuesto por tres partes y separadas por los simbolos "?" ":"
+        System.out.println("resultadoT = " + resultadoT);
+        
+        var numeroT = 7;
+        resultadoT = (numeroT % 2 == 0) ? "Es par" : "Es impar";
+        System.out.println("resultadoT = " + resultadoT);
+        */    
+        /*
+        var x = 5;
+        var y = 10;        
+        var z = ++x + y--; // En este caso veremos que a x se le sumara 1 y se sumara a y y luedo a y se le restara uno.
+        System.out.println("z = " + z);
+        // osea: priemro se realiza el incrememto de 'x', luego se suman '++x + y'
+        // y luego se realiza el decremento a y
+        
+        
+        var solucionAritmetica = 4 + 5 * 6 / 3; //El resultado sera 14 ya que el orden de la prioridad de los 
+        // operadores primero se haran las operaciones de multiplicacion de y de division, y luego
+        // se realizara la suma.
+        System.out.println("solucionAritmetica = " + solucionAritmetica);
+        
+        solucionAritmetica = (4 + 5) * 6 / 3; // en este caso se evaluara primero el parentesis
+        System.out.println("solucionAritmetica = " + solucionAritmetica);
+        */
+        
+        
+        
+        
+        
+        
+        
         
     }
 }
