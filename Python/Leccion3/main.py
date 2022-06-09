d929b620bba88a54db3e9d5f1da4aa3141be8713
Agustin Rodriguez <rodriguezalvarezagustin@gmail.com>
2022-06-09 02:42:00 -0300

    Ejercicios clase8 laboratorio

---
+++ Python/Leccion3/main.py
@@ -0,0 +1,21 @@
+# Ciclo while
+
+"""
+Es un ciclo que se repite mientras la condición sea verdadera
+"""
+"""
+contador = 0
+while contador < 3:
+    print('Ejecutamos nuestro ciclo while', contador)
+    contador += 1
+else:
+    print('Fin del ciclo while')
+"""
+
+# Imprimir los números del 0 al 5 con el ciclo while
+
+maximo = 5
+contador = 0
+while contador <= maximo:
+    print(contador)
+    contador += 1
